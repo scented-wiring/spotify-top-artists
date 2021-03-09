7457e1dbb6d882ab53b94a4c9bbba4080c1ea9f8
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-03-09 18:04:12 +0000

    add folder structure and basic styling

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div id="App">
+      <h1>
+        <i class="fab fa-spotify"></i>Spotify Top Artists
+      </h1>
+    </div>
+  );
+}
+
+export default App;
